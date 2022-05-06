2c3b90643599966bc6e2a4d4071673518225786c
prarathod <prajwal_fw11_331@masai.school>
2022-05-06 17:10:56 +0530

    factorial

--- test.js
+++ test.js
@@ -1,8 +1,26 @@
-let i =3;
-let j =4;
-let k =1;
-for(i=0;i<3;i++){
-    k+=j;
-    j=j-1;
+process.stdin.resume();
+
+var i = 0, t = 0;
+
+process.stdin.on('data', function (n) {
+    if (t == 0) {
+        t = n;
+    } else {
+        if (i++ < t) {
+            process.stdout.write(Factorial(n).toString());
+            if (i == t) {
+                process.exit();
+            }
+        } else {
+            process.exit();
+        }
+    }
+});
+
+function Factorial(n) {
+    if (n > 1) {
+        n *= Factorial(n - 1);
+    }
+
+    return n;
 }
-console.log(k)

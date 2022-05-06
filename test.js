6b0c11e90012e13035a3b6abda71834748491870
prarathod <prajwal_fw11_331@masai.school>
2022-05-06 22:37:40 +0530

    input boilercode

--- test.js
+++ test.js
@@ -1,26 +1,50 @@
-process.stdin.resume();
+// // process.stdin.resume();
 
-var i = 0, t = 0;
+// // var i = 0, t = 0;
 
-process.stdin.on('data', function (n) {
-    if (t == 0) {
-        t = n;
-    } else {
-        if (i++ < t) {
-            process.stdout.write(Factorial(n).toString());
-            if (i == t) {
-                process.exit();
-            }
-        } else {
-            process.exit();
-        }
-    }
-});
+// // process.stdin.on('data', function (n) {
+// //     if (t == 0) {
+// //         t = n;
+// //     } else {
+// //         if (i++ < t) {
+// //             process.stdout.write(anagram(n).toString());
+// //             if (i == t) {
+// //                 process.exit();
+// //             }
+// //         } else {
+// //             process.exit();
+// //         }
+// //     }
+// // });
 
-function Factorial(n) {
-    if (n > 1) {
-        n *= Factorial(n - 1);
-    }
+// // function anagram(n) {
+// //     if (n > 1) {
+// //         n *= anagram(n - 1);
+// //     }
+// //     return n;
+// // }
 
-    return n;
-}
+
+
+
+
+
+
+// // Sample code to perform I/O:
+
+// process.stdin.resume();
+// process.stdin.setEncoding("utf-8");
+// var stdin_input = "";
+
+// process.stdin.on("data", function (input) {
+//     stdin_input += input;                               // Reading input from STDIN
+// });
+
+// process.stdin.on("end", function () {
+//    main(stdin_input);
+// });
+
+// function main(input) {
+//     process.stdout.write("Hi, " + input + ".\n");       
+
+// }

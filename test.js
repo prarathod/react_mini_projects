1de4d367eca75c103be5c6e20c323438d61a79dc
prarathod <prajwal_fw11_331@masai.school>
2022-05-09 13:03:27 +0530

    input boiler code

--- test.js
+++ test.js
@@ -1,50 +1,15 @@
-// // process.stdin.resume();
+process.stdin.resume();
 
-// // var i = 0, t = 0;
+var t = '';
 
-// // process.stdin.on('data', function (n) {
-// //     if (t == 0) {
-// //         t = n;
-// //     } else {
-// //         if (i++ < t) {
-// //             process.stdout.write(anagram(n).toString());
-// //             if (i == t) {
-// //                 process.exit();
-// //             }
-// //         } else {
-// //             process.exit();
-// //         }
-// //     }
-// // });
-
-// // function anagram(n) {
-// //     if (n > 1) {
-// //         n *= anagram(n - 1);
-// //     }
-// //     return n;
-// // }
-
-
-
-
-
-
-
-// // Sample code to perform I/O:
-
-// process.stdin.resume();
-// process.stdin.setEncoding("utf-8");
-// var stdin_input = "";
-
-// process.stdin.on("data", function (input) {
-//     stdin_input += input;                               // Reading input from STDIN
-// });
-
-// process.stdin.on("end", function () {
-//    main(stdin_input);
-// });
-
-// function main(input) {
-//     process.stdout.write("Hi, " + input + ".\n");       
-
-// }
+process.stdin.on('data', function (n) {
+    if (t == 0) {
+        t = n;
+    } else {
+        if (t != "") {
+            process.stdout.write(sum(t.toString().trim(), n.toString().trim()));
+        } else {
+            process.exit();
+        }
+    }
+});
